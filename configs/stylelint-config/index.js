94bdeef587a753ce5adbf93496cf33940ae33246
mjeightyfive <mjeightyfive@gmail.com>
2022-01-01 08:31:00 +0000

    tweak stylelint rules and fix linting

--- configs/stylelint-config/index.js
+++ configs/stylelint-config/index.js
@@ -1,16 +1,17 @@
 module.exports = {
     extends: 'stylelint-config-standard',
     rules: {
-        'at-rule-no-unknown': [
-            true,
-            {
-                ignoreAtRules: ['include', 'media', 'mixin']
-            }
-        ],
         'color-named': 'always-where-possible',
         'declaration-no-important': true,
         indentation: 4,
         'string-quotes': 'single',
-        'alpha-value-notation': 'number'
+        'alpha-value-notation': 'number',
+        'custom-property-empty-line-before': null,
+        'value-keyword-case': null,
+        'declaration-colon-newline-after': null,
+        'value-list-comma-newline-after': null,
+        'max-line-length': 140,
+        'number-max-precision': 5,
+        'selector-pseudo-class-no-unknown': null
     }
 };
